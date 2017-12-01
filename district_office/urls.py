e5448c04b5619c2503d10e9e8e3cd02f124b4abf
Piotr Chmielewski <pchmielewski6@gmail.com>
2017-12-01 20:27:52 +0100

    rebase with register car

--- district_office/urls.py
+++ district_office/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   url(r'^$', views.CarListView.as_view(), name='index'),
   url(r'^(?P<pk>[A-Z0-9]+)/detail/$', views.CarDetailView.as_view(), name='detail'),
   url(r'^add/$', views.CarAdd.as_view(), name='add'),
+  url(r'^card/(?P<car_id>[A-Z0-9]+)/$', views.card_generation_view, name='card'),
 ]
--- district_office/views.py
+++ district_office/views.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.views.generic.edit import CreateView
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponse
+
+from reportlab.pdfgen import canvas
 from .models import Car
 
 class CarListView(generic.ListView):
@@ -24,3 +27,19 @@ class CarAdd(CreateView):
     fields=['vin','owner','reg_no','model','mark','production_year',
 	'engine_number','engine_capacity','engine_power','last_tech_exam']
     success_url = '/office/'
+
+def card_generation_view(request, car_id):
+  car = Car.objects.get(pk=car_id)
+  response = HttpResponse(content_type='application/pdf')
+  response['Content-Disposition'] = 'attachment; filename="'+car_id+'.pdf"'
+  p = canvas.Canvas(response)
+  offset = 100
+  fields = car.__dict__
+  for field, value in fields.items():
+    if (field == "_state"):
+      continue
+    p.drawString(100, offset, str(field) + " - " + str(value))
+    offset = offset + 50
+  p.showPage()
+  p.save()
+  return response
